ce9f4d08e8b5c1932bf5cc6454019ed196aeefa6
loopfagundes <lof.fagundes@gmail.com>
2024-07-09 12:13:33 -0300

    update/fix swaggerConfig, cleans comments in pom.

--- src/main/java/br/dev/swagger/config/SwaggerConfig.java
+++ src/main/java/br/dev/swagger/config/SwaggerConfig.java
@@ -6,10 +6,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public OpenAPI userOpenAPI() {
