5d5a4d7262646e64728da037b2c15a3166a11e84
loopfagundes <lof.fagundes@gmail.com>
2024-07-08 21:56:46 -0300

    completed basic API with swagger openAPI

--- src/main/java/br/dev/swagger/models/Usuario.java
+++ src/main/java/br/dev/swagger/models/Usuario.java
@@ -3,7 +3,6 @@ package br.dev.swagger.models;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
@@ -23,7 +22,7 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank
+    @NotBlank(message = "Nome nao pode ser em branco.")
     @Column(name = "nome")
     private String nome;
 
